907022393871d2287aa77ac2b7f0395e77b9feed
Arsany Samuel <arsanysamuel.as@gmail.com>
2022-09-20 17:37:55 +0200

    database url to environment variable

--- flaskr/config.py
+++ flaskr/config.py
@@ -1,10 +1,12 @@
+import os
 import credentials
 
 # Database Variables
 HOST = "localhost"
 PORT = 3306
 DB_FILE = "riders"
-DATABASE_URI = f"mariadb+pymysql://{credentials.db_username}:{credentials.db_password}@{HOST}:{PORT}/{DB_FILE}?charset=utf8mb4"  # will be created in datadir of mariadb, in my case /var/lib/mysql
+# DATABASE_URI = f"mariadb+pymysql://{credentials.db_username}:{credentials.db_password}@{HOST}:{PORT}/{DB_FILE}?charset=utf8mb4"  # will be created in datadir of mariadb, in my case /var/lib/mysql
+DATABASE_URI = os.environ["DATABASE_URL"]
 
 # Regex
 REGEX = {
